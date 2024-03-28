83d99dc5c6388d3e5b8603e5d19ff092c07908ae
payal vasave <udayvasave94@gmail.com>
2024-03-28 16:45:02 +0530

    added home page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import NavBar from './Components/NavBar'
+import Home from './Components/Home'
 
 
 const App = () => {
   return (
     <>
-<NavBar />
+    <Home />
     </>
   )
 }
