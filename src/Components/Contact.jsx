145c46f64f5d7e57a20667bbdb50d40aeb576ca3
ankitachaudhari123 <ankitachaudhari631@gmail.com>
2024-04-03 15:52:00 +0530

    updated

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -1,7 +1,26 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import './Contact.css'
+import emailjs from '@emailjs/browser';
 
 const Contact = () => {
+    const form = useRef();
+    const sendEmail = (e) => {
+        e.preventDefault();
+    
+        emailjs
+          .sendForm('service_60lxjgt', 'template_pwrcfr1', form.current, {
+            publicKey: 'HevVQmrZfqOBZ3NnWTBex',
+          })
+          .then(
+            () => {
+              console.log('SUCCESS!');
+            },
+            (error) => {
+              console.log('FAILED...', error.text);
+            },
+          );
+      };
+    
   return (
     <>
     <section id='contact'>
@@ -16,7 +35,20 @@ const Contact = () => {
                 </div>
             </div>
             <div className='contact-div'>
-
+                <center>
+                <div className="contact-text-div">
+                    <p className='contact-text'>Please fill out the form below to discuss any work opportunities.</p>
+                </div>
+                
+                <div className="contact-from-div">
+                    <form className="contact-form" ref={form} onSubmit={sendEmail}>
+                        <input type="text" className='name' placeholder='Your Name' name="user_name"/>
+                        <input type="email" className='email' placeholder='Your Email' name="user_email"/>
+                        <textarea className='msg' name='message' rows="5" placeholder='Your Message'></textarea>
+                        <button type="submit" value="send" className="submitBtn">Submit</button>
+                    </form>
+                </div>
+                </center>
             </div>
         </div>
     </section>
