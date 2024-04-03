03f98b9c064a62abd0e7f80ad33acd292dea41e0
ankitachaudhari123 <ankitachaudhari631@gmail.com>
2024-04-03 16:44:31 +0530

    icon added

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,5 +1,13 @@
 import React from 'react'
 import './NavBar.css'
+import { IoHome } from "react-icons/io5";
+import { FaUser } from "react-icons/fa";
+import { IoLogoLinkedin } from "react-icons/io5";
+import { TbBrandGithubFilled } from "react-icons/tb";
+import { AiFillInstagram } from "react-icons/ai";
+import { FaPhone } from "react-icons/fa6";
+import { FaAward } from "react-icons/fa";
+import { FaLaptopCode } from "react-icons/fa";
 
 const NavBar = () => {
   return (
@@ -7,17 +15,17 @@ const NavBar = () => {
       <ul>
         <br></br>
         <br></br>
-        <a href='#home' className='a-hrf'><li className='nav-bar-items'>Home</li></a>
-        <a href='#about' className='a-hrf'><li className='nav-bar-items'>About</li></a>
-        <a href='#project' className='a-hrf'><li className='nav-bar-items'>Project</li></a>
-        <a href='#experience' className='a-hrf'><li className='nav-bar-items'>Experience</li></a>
-        <a href='#contact' className='a-hrf'><li className='nav-bar-items'>Contact</li></a>
+        <a href='#home' className='a-hrf'><li className='nav-bar-items'><IoHome />Home</li></a>
+        <a href='#about' className='a-hrf'><li className='nav-bar-items'><FaUser />About</li></a>
+        <a href='#project' className='a-hrf'><li className='nav-bar-items'><FaLaptopCode />Project</li></a>
+        <a href='#experience' className='a-hrf'><li className='nav-bar-items'><FaAward />Experience</li></a>
+        <a href='#contact' className='a-hrf'><li className='nav-bar-items'><FaPhone />Contact</li></a>
       </ul>
       <h5 className='nav-bar-text'>Socials</h5>
       <ul>
-      <a href='https://www.linkedin.com/in/ankita-chaudhari-6650b3273/' className='a-hrf'><li className='nav-bar-social-items'>Linkedin</li></a>
-      <a href='https://github.com/ankitachaudhari123' className='a-hrf'><li className='nav-bar-social-items'>GitHub</li></a>
-      <a href='https://www.instagram.com/an_kita_30/' className='a-hrf'><li className='nav-bar-social-items'>Instragram</li></a>
+      <a href='https://www.linkedin.com/in/ankita-chaudhari-6650b3273/' className='a-hrf'><li className='nav-bar-social-items'><IoLogoLinkedin />Linkedin</li></a>
+      <a href='https://github.com/ankitachaudhari123' className='a-hrf'><li className='nav-bar-social-items'><TbBrandGithubFilled />GitHub</li></a>
+      <a href='https://www.instagram.com/an_kita_30/' className='a-hrf'><li className='nav-bar-social-items'><AiFillInstagram />Instragram</li></a>
       </ul>
     </div>
   )
