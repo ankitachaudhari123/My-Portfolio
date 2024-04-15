36ba3acce97e876bd01c04591e66f43d135ff740
ankitachaudhari123 <ankitachaudhari631@gmail.com>
2024-04-15 22:09:23 +0530

    updated

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -5,10 +5,8 @@ const Home = () => {
     <>
     <section id='home'>
       <div className='home-section'>
-      <div className="mobile-view-nav-bar-div">
-
-      </div>
       <center>
+        <br></br>
       <div className="self-info-div">
         <h1 className="self-name">Hey, I'm Ankita</h1>
         <p className='self-qualification-into'>Full Stack Web Developer</p>
